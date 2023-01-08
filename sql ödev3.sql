54211e5f2bc8672826d803cc8a5868ecb560ad32
zeos81 <105157722+zeos81@users.noreply.github.com>
2023-01-08 16:58:30 +0300

    Update sql ödev3.sql

--- sql ödev3.sql
+++ sql ödev3.sql
@@ -7,7 +7,7 @@ country tablosunda bulunan country sütunundaki ülke isimlerinden en az 6 karak
 film tablosunda bulunan title sütunundaki film isimlerinden en az 4 adet büyük ya da küçük harf farketmesizin 'T' karakteri içeren film isimlerini sıralayınız.
 film tablosunda bulunan tüm sütunlardaki verilerden title 'C' karakteri ile başlayan ve uzunluğu (length) 90 dan büyük olan ve rental_rate 2.99 olan verileri sıralayınız.
 Kolay Gelsin.
- 
+ */
 
 -- 1
 SELECT country FROM country WHERE country LIKE 'A%a';
